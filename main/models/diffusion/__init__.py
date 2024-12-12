88f6962663ff19d76674447494ac022a75f50b54
Yunjeong Im <yjim.do@gmail.com>
2024-12-13 04:45:01 +0900

    Update __init__.py
    Add joint_wrapper module

--- main/models/diffusion/__init__.py
+++ main/models/diffusion/__init__.py
@@ -2,5 +2,6 @@ from .unet_openai import UNetModel, SuperResModel
 from .ddpm import DDPM
 from .ddpm_form2 import DDPMv2
 from .wrapper import DDPMWrapper
+from .joint_wrapper import JointWrapper
 from .spaced_diff import SpacedDiffusion
 from .spaced_diff_form2 import SpacedDiffusionForm2
